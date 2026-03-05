08aa28018b22513278b06475dd0c3686fce0c345
Jekabs Vidrusks <jekabs.vidrusks@gmail.com>
2026-03-05 09:46:08 +0200

    refactoring

--- ECS/StartState.cpp
+++ ECS/StartState.cpp
@@ -3,6 +3,41 @@
 #include "MeshComponent.h"
 #include "SimpleSprite.h"
 
+class ProcessWASD : public InputSystem::Processor
+{
+public:
+    ProcessWASD(std::string _name) : InputSystem::Processor(_name) {};
+    int Process(InputSystem::INPUT_DATA_4& data) override
+    {
+        float x = data[3] - data[1];
+        float y = data[2] - data[0];
+        float lengthSq = x * x + y * y;
+        if (lengthSq > 0.0001f)
+        {
+            float length = std::sqrt(lengthSq);
+            x /= length;
+            y /= length;
+        }
+        data[0] = x;
+        data[1] = y;
+        data[2] = 0.0f;
+        data[3] = 0.0f;
+
+        return 0;
+    }
+};
+
+class ProcessQE : public InputSystem::Processor
+{
+public:
+    ProcessQE(std::string _name) : InputSystem::Processor(_name) {};
+    int Process(InputSystem::INPUT_DATA_4& data) override
+    {
+        data[0] -= data[1];
+        return 0;
+    }
+};
+
 enum Mesh_names {
     TRIANGLE = 0,
     SQUARE = 1
@@ -11,20 +46,13 @@ enum Mesh_names {
 ecs::Entity e;
 ecs::Entity player;
 
-float speedy;
-float speedx;
-
 void StartState::Init()
 {
-
     Mesh triangleMesh = {
         {-1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f},
         {1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f},
         {0.0f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f} 
     };
-
-    _data->assets.AddMesh(0, triangleMesh);
-
     Mesh squareMesh = {
     {{-1,  1}, {1.0f, 0.0f, 0.0f, 1.0f}},
     {{-1, -1}, {0.0f, 1.0f, 0.0f, 1.0f}},
@@ -32,90 +60,72 @@ void StartState::Init()
 
     {{ 1, -1}, {1.0f, 1.0f, 0.0f, 1.0f}},
     {{-1, -1}, {0.0f, 1.0f, 0.0f, 1.0f}},
-    {{ 1,  1}, {0.0f, 0.0f, 1.0f, 1.0f}} 
+    {{ 1,  1}, {0.0f, 0.0f, 1.0f, 1.0f}}
     };
 
-    _data->assets.AddMesh(1, squareMesh);
 
-
-
-
-    e = ecs.create_entity();
-    ecs.add_component(e, RenderComponent(false));
-    ecs.add_component(e, MeshComponent(TRIANGLE, -1));
-    
-    player = ecs.create_entity();
-    ecs.add_component(player, RenderComponent(true));
-    ecs.add_component(player, SimpleSprite({100, 100, 100, 100}, {0, 0, 64, 64}, 2));
+    _data->assets.AddMesh(TRIANGLE, triangleMesh);
+    _data->assets.AddMesh(SQUARE, squareMesh);
 
     _data->assets.LoadBMPTexture(1, "../image.bmp", _data->renderer);
     _data->assets.LoadBMPTexture(2, "../player.bmp", _data->renderer);
 
-    RenderComponent& rc = ecs.get<RenderComponent>(e);
-    rc.scale = { 10, 10 };
-    rc.Position = { 10, 10 };
+    e = ecs.create_entity();
+    player = ecs.create_entity();
 
+    ecs.add_component(e, RenderComponent(false));
+    ecs.add_component(e, MeshComponent(TRIANGLE, -1));
+    
+    ecs.add_component(player, RenderComponent(true, Vec2(10, 10), Vec2(1, 1)));
+    ecs.add_component(player, SimpleSprite({100, 100, 100, 100}, {0, 0, 64, 64}, 2));
+
+
+    _data->inputs.AddActionMap("gameplay").AddAction("move")
+        .AddBinding(InputSystem::Button, InputSystem::Keyboard, SDL_SCANCODE_W, 0)
+        .AddBinding(InputSystem::Button, InputSystem::Keyboard, SDL_SCANCODE_A, 1)
+        .AddBinding(InputSystem::Button, InputSystem::Keyboard, SDL_SCANCODE_S, 2)
+        .AddBinding(InputSystem::Button, InputSystem::Keyboard, SDL_SCANCODE_D, 3)
+        .AddProcessor(std::make_unique<ProcessWASD>("wasd"));
+
+    _data->inputs.AssignDeviceToPlayer(player, InputSystem::KeyboardHub::Current());
+    _data->inputs.AssignMapToPlayer(player, "gameplay");
+
+    _data->inputs.GetActionMap("gameplay").AddAction("scale")
+        .AddBinding(InputSystem::Button, InputSystem::Keyboard, SDL_SCANCODE_Q, 0)
+        .AddBinding(InputSystem::Button, InputSystem::Keyboard, SDL_SCANCODE_E, 1)
+        .AddProcessor(std::make_unique<ProcessQE>("qe"));
 }
 
-float dx = 0;
-float dy = 0;
-bool direction = 0;
+
+
+
 int TexX;
 int TexY;
+bool direction = 0;
 
 Uint32 start = SDL_GetTicks();
 
-
-
-
 void StartState::Update()
 {
-    SDL_Event event;
-    while (SDL_PollEvent(&event))
-    {
-        if (event.type == SDL_EVENT_QUIT)
-            _data->quit = true;
-    }
-
-    const bool* key_states = SDL_GetKeyboardState(NULL);
     RenderComponent& rc = ecs.get<RenderComponent>(player);
 
-    dx = 0;
-    dy = 0;
-    if (key_states[SDL_SCANCODE_W]) {
-        dy -= 1;
-    }
-    if (key_states[SDL_SCANCODE_S]) {
-        dy += 1;
-    }
-    if (key_states[SDL_SCANCODE_A]) {
-        dx -= 1;
-    }
-    if (key_states[SDL_SCANCODE_D]) {
-        dx += 1;
-    }
-    if (key_states[SDL_SCANCODE_E]) {
-        rc.scale.x -= 1.0f;
-        rc.scale.y -= 1.0f;
-    }
-    if (key_states[SDL_SCANCODE_Q]) {
-        rc.scale.x += 1.0f;
-        rc.scale.y += 1.0f;
-    }
+    InputSystem::INPUT_DATA_4 dMove = _data->inputs.GetActionAxis("move", player);
+    InputSystem::INPUT_DATA_4 dScale = _data->inputs.GetActionAxis("scale", player);
+
+    rc.position.x += 10 * dMove[0];
+    rc.position.y += 10 * dMove[1];
+
+    rc.scale.x += dScale[0];
+    rc.scale.y += dScale[0];
 
 
-    rc.Position.x += 10 * dx;
-    rc.Position.y += 10 * dy;
-
-    
-
-    if (dx < 0)
+    if (dMove[0] < 0)
         direction = 1;
-    if (dx > 0)
+    if (dMove[0] > 0)
         direction = 0;
     
     
-    if (!dx)
+    if (!dMove[0])
     {
         if (!direction)
         {
@@ -145,45 +155,16 @@ void StartState::Update()
             TexX %= 8;
         }
     }
-    
         
 
 
     SimpleSprite& ss = ecs.get<SimpleSprite>(player);
     ss.TextureRect.x = TexX * 64;
     ss.TextureRect.y = TexY * 64;
-    /*rc.Position.x;
-    rc.Position.y;*/
-
-
-    /*if (rc.Position.x + rc.scale.x >= 1920 || rc.Position.x - rc.scale.x <= 0)
-        speedx *= -1;
-    if (rc.Position.y + rc.scale.y >= 1080 || rc.Position.y - rc.scale.y <= 0)
-        speedy *= -1;*/
-
-
-    //if (key_states[SDL_SCANCODE_UP]) {
-    //    if(speedx < 0)
-    //        speedx -= 0.1f;
-    //    else speedx += 0.1f;
-    //    if (speedy < 0)
-    //        speedy -= 0.1f;
-    //    else speedy += 0.1f;
-    //}
-    //if (key_states[SDL_SCANCODE_DOWN]) {
-    //    if (speedx > 0)
-    //        speedx -= 0.1f;
-    //    else speedx += 0.1f;
-    //    if (speedy > 0)
-    //        speedy -= 0.1f;
-    //    else speedy += 0.1f;
-    //    /*speedx -= 0.1f;
-    //    speedy -= 0.1f;*/
-    //}
-
-
-    
 }
+
+
+
 
 void StartState::Render()
 {
@@ -208,8 +189,8 @@ void StartState::Render()
                         SDL_FRect RectToDraw = ss->rect;
                         RectToDraw.h *= rc->scale.y;
                         RectToDraw.w *= rc->scale.x;
-                        RectToDraw.x += rc->Position.x;
-                        RectToDraw.y += rc->Position.y;
+                        RectToDraw.x += rc->position.x;
+                        RectToDraw.y += rc->position.y;
 
                         if(!ss->TextureRect.h || !ss->TextureRect.w)
                             SDL_RenderTexture(_data->renderer, _data->assets.GetTexture(ss->TextureId), nullptr, &RectToDraw);
@@ -225,10 +206,10 @@ void StartState::Render()
                             for (int i = 0; i < mesh_toDraw.size(); i++)
                             {
                                 mesh_toDraw[i].position.x *= rc->scale.x;
-                                mesh_toDraw[i].position.x += rc->Position.x;
+                                mesh_toDraw[i].position.x += rc->position.x;
 
                                 mesh_toDraw[i].position.y *= rc->scale.y;
-                                mesh_toDraw[i].position.y += rc->Position.y;
+                                mesh_toDraw[i].position.y += rc->position.y;
 
                                 mesh_toDraw[i].color;
                                 mesh_toDraw[i].tex_coord;
