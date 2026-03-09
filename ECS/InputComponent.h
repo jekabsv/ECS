c788609df4203b81d365503ab5b0c6b6993729fe
jekabs <jekabs.vidrusks@gmail.com>
2026-03-09 09:37:09 +0200

    Revert "hear me out"
    This reverts commit 6f1246e586e93a0fb80b4071db08c1587dcfd343.

--- ECS/InputComponent.h
+++ ECS/InputComponent.h
@@ -6,4 +6,26 @@
 struct InputComponent
 {
 	uint32_t PlayerID;
+	InputSystem::System& system;
+	InputComponent(uint32_t player, InputSystem::System& data) : PlayerID(player), system(data) {};
+	const InputSystem::INPUT_DATA_4 GetActionAxis(StringId ActionName)
+	{
+		auto it = system.playerActionStates.find(PlayerID);
+		if (it == system.playerActionStates.end())
+			return { 0, 0, 0, 0 };
+		auto it2 = it->second.find(ActionName);
+		if (it2 != it->second.end())
+			return it2->second.data;
+		return {0, 0, 0, 0};
+	}
+	const InputSystem::ActionState GetActionState(StringId ActionName)
+	{
+		auto it = system.playerActionStates.find(PlayerID);
+		if (it == system.playerActionStates.end())
+			return InputSystem::Idle;
+		auto it2 = it->second.find(ActionName);
+		if (it2 != it->second.end())
+			return it2->second.state;
+		return InputSystem::Idle;
+	}
 };
